1e3a6e47ef7dc404271111352505681e40f56a05
Ezequiel Figueiredo Dawud <126517272+ezequiel-dawud@users.noreply.github.com>
2023-09-19 15:21:31 -0300

    atualização

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,4 +1,28 @@
 var botaoAdicionar = document.querySelector("#buscar-paciente")
 botaoAdicionar.addEventListener("click", () => {
     console.log("buscando pacientes")
+
+    var xhr = new XMLHttpRequest()
+
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json")
+
+    xhr.addEventListener("load", () => {
+        var erroAjax = document.querySelector("#erro-ajax")
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel")
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta)
+            pacientes.forEach((paciente) => {
+                adicionaPacienteNaTabela(paciente)
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel")
+        }
+
+    })
+
+    xhr.send()
 })
+
